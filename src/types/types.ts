d2e8bb2b8dc0c13cd0f9e843426ae5b0871f86df
Bina  Hovav <binahovav@MacBook-Pro-Bina.local>
2024-08-06 11:01:25 +0300

    first commit

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+
+export enum DaysOfWeek {
+  Sunday = 'Sunday',
+  Monday = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+}
+
+export const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snacks", "Notes"];
